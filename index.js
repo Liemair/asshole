dc1f87e68c0785bb654641950e285145e705e47f
liemair <pachump001@gmail.com>
2020-08-24 15:45:21 -0700

    data base test table

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const cool = require('cool-ascii-faces');
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -7,6 +14,18 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .get('/times', (req, res) => res.send(showTimes()))
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
